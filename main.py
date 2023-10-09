6ab76f27e893c7f5cc8059ac1a316c86c93f32e0
Artem Usachov <ar.usa4yov@yandex.ru>
2023-10-09 16:42:56 +0300

    grammar fixes

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import math
 
 class Figure:
     def square(self):
-        raise NotImplementedError('Method "square" should be used to subclasses')
+        raise NotImplementedError('Method "square" should be used to subclasses!')
 
 
 class Circle(Figure):
     def __init__(self, radius):
 
         if radius < 0:
-            raise ValueError("Radius should be non-negative number!")
+            raise ValueError("Radius should be a non-negative number!")
 
         self.radius = radius
 
@@ -22,7 +22,7 @@ class Triangle(Figure):
 
     def __init__(self, first_side, second_side, third_side):
         if first_side < 0 or second_side < 0 or third_side < 0:
-            raise ValueError("Side should be non-negative number!")
+            raise ValueError("Side should be a non-negative number!")
 
         if not (first_side + second_side > third_side
                 and first_side + third_side > second_side
@@ -45,5 +45,5 @@ class Triangle(Figure):
                or self.second_side ** 2 + self.third_side ** 2 == self.first_side ** 2
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     pass
